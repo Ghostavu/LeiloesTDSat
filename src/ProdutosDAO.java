7b20349f18464581a37b7112768d3e0f3ed6513b
Ghostavu <gkisner33@gmail.com>
2026-03-08 11:56:42 -0300

    Criada função listarProdutosVendidos no ProdutosDAO

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -13,7 +13,7 @@ public class ProdutosDAO {
     
     public void cadastrarProduto (ProdutosDTO produto){
         
-        //conn = new conectaDAO().connectDB();
+        // conn = new conectaDAO().connectDB();
         
     }
     
@@ -21,21 +21,51 @@ public class ProdutosDAO {
         
         return listagem;
     }
-    
+
     public void venderProduto(int idProduto) {
 
-    String sql = "UPDATE produtos SET status = 'Vendido' WHERE id = ?";
+        String sql = "UPDATE produtos SET status = 'Vendido' WHERE id = ?";
 
-    try {
+        try {
 
-        conn = new conectaDAO().connectDB();
-        PreparedStatement stmt = conn.prepareStatement(sql);
+            conn = new conectaDAO().connectDB();
+            PreparedStatement stmt = conn.prepareStatement(sql);
 
-        stmt.setInt(1, idProduto);
-        stmt.executeUpdate();
+            stmt.setInt(1, idProduto);
+            stmt.executeUpdate();
 
-    } catch (Exception e) {
-        e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-}
+
+    public ArrayList<ProdutosDTO> listarProdutosVendidos(){
+
+        ArrayList<ProdutosDTO> lista = new ArrayList<>();
+
+        String sql = "SELECT * FROM produtos WHERE status = 'Vendido'";
+
+        try {
+
+            conn = new conectaDAO().connectDB();
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+
+            while(rs.next()){
+
+                ProdutosDTO produto = new ProdutosDTO();
+
+                produto.setId(rs.getInt("id"));
+                produto.setNome(rs.getString("nome"));
+                produto.setStatus(rs.getString("status"));
+
+                lista.add(produto);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return lista;
+    }
 }
